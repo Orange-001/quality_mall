b4d5815e59c868f7285c52e9e95c5c58d57351f0
Orange-001 <1347680852@qq.com>
2021-10-07 16:46:47 +0800

    实现个人中心->我的订单

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -20,5 +20,30 @@ Page({
     } catch (error) {
       console.log(error);
     }
-  }
+  },
+
+  /* getUserProfile(){
+    wx.getUserProfile({
+      desc: '登录',
+      success: async (res)=>{
+        // 1 获取用户信息
+        console.log(res)
+        const { encryptedData, rawData, iv, signature } = res;
+        // 2 获取小程序登录成功后的code
+        const { code } = await login();
+        const loginParams={ encryptedData, rawData, iv, signature ,code};
+        //  3 发送请求 获取用户的token
+        const {token}=await request({url:"/users/wxlogin",data:loginParams,method:"post"});
+        // 4 把token存入缓存中 同时跳转回上一个页面
+        wx.setStorageSync("token", token);
+        wx.navigateBack({
+          delta: 1  
+        });
+      },
+      fail: (err)=>{
+        console.log(err)
+      }
+    })
+  } */
+
 })
